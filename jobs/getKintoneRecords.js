286c8554d058b471f89047d236a90f66462518a1
tetrapod418 <hikeybow@gmail.com>
2023-10-14 15:01:50 +0900

    change update to record status.

--- jobs/getKintoneRecords.js
+++ jobs/getKintoneRecords.js
@@ -31,22 +31,17 @@ function getQRCodeUrl(url) {
       const resp = await client.record.getRecords(params);
       console.log(resp.records);
       const arrayOfLists = resp.records.map(
-        record => <dl><dt>{record.title.value}</dt><dd className="ddimg"><img src={getQRCodeUrl(record.URL.value)} alt={record.URL.value} title={record.title.value} /> </dd><dd className="descriptions">{record.descriptions.value}</dd></dl>
-        )
         // 取得レコードのステータス更新
-        const updateParams = {
-          app: APP_ID,
-          id: record.id.value,
-          record: {
-            'ステータス': {
-              'value': 'published'
-            }
-          }
-        }
-        console.log(`record.id.value = ${record.id.value}`);
-          const updated = await client.record.updateRecord(2,updateParams)
-        //return arrayOfLists;
+        await client.record.updateRecord(2,
+            {app: APP_ID, id: record.id.value, 
+              record: {
+                'ステータス': {
+                  'value': 'published'
+                }
     
+              }})
+        );
+  
     } catch (err) {
       console.log(err);
     }
